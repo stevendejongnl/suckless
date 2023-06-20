29e8bf4187f3579e1f05dfa35459eebc815a41bc
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2023-06-20 18:54:21 +0200

    Update with styling and keymap

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 1;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -10,17 +10,21 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+static const char *fonts[]          = { "Wuncon Siji:size=10", "xos4 Terminus:size=9" };
+// static const char *fonts[]          = { "monospace:size=10" };
+// static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "xos4 Terminus:size=9";
+
+static const char col_text1[]       = "#cdd6f4";
+static const char col_text2[]       = "#bbbbbb";
+static const char col_surface1[]    = "#313244";
+static const char col_surface2[]    = "#585b70";
+static const char col_surface3[]    = "#45475a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_text2, col_surface2, col_surface3 },
+	[SchemeSel]  = { col_text1, col_surface1,  col_surface1 },
 };
 
 /* tagging */
@@ -33,7 +37,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Kitty",    NULL,       NULL,       0,            0,           -1 },
-	{ "Brave",    NULL,       NULL,       1 << 8,       0,           -1 },
+	// { "Brave",    NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -44,9 +48,12 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{"\uE005" , tile}    ,
+  {"\uE011" , NULL}    ,
+  {"[M]"    , monocle} ,
+	// { "[]=",      tile },    /* first entry is default */
+	// { "><>",      NULL },    /* no layout function means floating behavior */
+	// { "[M]",      monocle },
 };
 
 /* key definitions */
@@ -62,7 +69,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_surface2, "-nf", col_text2, "-sb", col_surface1, "-sf", col_text1, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 #include "shift-tools.c"
 
@@ -73,7 +80,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_i,	shiftview,           { .i = -1 } },
  	{ MODKEY,	                      XK_i, shiftviewclients,    { .i = -1 } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_space,  spawn,          SHCMD ("$HOME/.config/rofi/launch.sh")},
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -85,13 +93,13 @@ static const Key keys[] = {
 	{ MODKEY|ControlMask,		        XK_l,      shiftswaptags,  { .i = +1 }	},
 	{ MODKEY|ShiftMask,             XK_l,      shiftboth,      { .i = +1 }	},
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ControlMask,           XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
@@ -99,6 +107,20 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+
+	// XF86MonBrightnessDown
+  {0, 														0x1008ff02, spawn,  SHCMD ("xbacklight -inc 5")},
+	// XF86MonBrightnessUp
+  {0, 														0x1008ff03, spawn,    SHCMD ("xbacklight -dec 5")},
+	// XF86AudioLowerVolume
+  {0,															0x1008ff11, spawn,   SHCMD ("pamixer --decrease 5")},
+  // XF86AudioMute
+	{0,															0x1008ff12, spawn,					 SHCMD ("pamixer --toggle-mute")},
+  // XF86AudioRaiseVolume
+	{0,															0x1008ff13, spawn,   SHCMD ("pamixer --increase 5")},
+  // XF86AudioMicMute
+	// {0															XF86AudioMicMute, spawn,       SHCMD ("pamixer --default-source --toggle-mute")},
+
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
