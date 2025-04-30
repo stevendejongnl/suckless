84f7d62477de4efead8df2f16bb866403982ed54
Steven de jong 🍟 <sdejong@cloudsuite.com>
2025-04-30 10:13:32 +0200

    DWM focusonclick

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -10,6 +10,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int focusonwheel       = 0;
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -82,6 +83,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", "-e", "tmux", "new-session", "-A", "-s", "main", NULL };
+static const char *termtwocmd[]  = { "alacritty", "-e", "tmux", NULL };
 #include "shift-tools.c"
 
 static const Key keys[] = {
@@ -97,6 +99,7 @@ static const Key keys[] = {
   { MODKEY,                       XK_space,  spawn,          SHCMD("rofi -show drun") },
   { MODKEY|Mod1Mask,              XK_space,  spawn,          SHCMD("rofi -show window") },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|Mod1Mask,              XK_Return, spawn,          {.v = termtwocmd } },
 
 	// Toggle bar
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
